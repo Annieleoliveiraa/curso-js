4989a200eb38ec76d6372fff2c3ea08bc8fc7c50
Anniele Oliveiraa <annieleoliveiraa@gmail.com>
2024-12-10 13:42:17 -0300

    feat: Adiciona o js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var nome = prompt("Qual seu nome?")
+var idade = Number(prompt("Qual é sua idade?"))
+
+
+document.write("<h1>Bem vindo " + nome + ", você tem " + idade + " anos de idade" +  
+"</h1>")
